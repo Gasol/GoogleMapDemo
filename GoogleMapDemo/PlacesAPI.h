3286c8423e0274f6b1b958e73d6559a4eddb0961
Gasol Wu <gasol.wu@gmail.com>
2011-10-23 01:42:46 +0800

    new PlacesAPI.{h,m}

---
+++ GoogleMapDemo/PlacesAPI.h
@@ -0,0 +1,13 @@
+//
+//  PlacesAPI.h
+//  GoogleMapDemo
+//
+//  Created by Gasol Wu on 10/23/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlacesAPI : NSObject
+
+@end
---
+++ GoogleMapDemo/PlacesAPI.m
@@ -0,0 +1,13 @@
+//
+//  PlacesAPI.m
+//  GoogleMapDemo
+//
+//  Created by Gasol Wu on 10/23/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PlacesAPI.h"
+
+@implementation PlacesAPI
+
+@end
