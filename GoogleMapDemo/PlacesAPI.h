3d305cd35048c3358e44d22c69927e5bcd65254a
Gasol Wu <gasol.wu@gmail.com>
2011-10-26 23:52:42 +0800

    PlacesAPI implementation

--- GoogleMapDemo/PlacesAPI.h
+++ GoogleMapDemo/PlacesAPI.h
@@ -8,6 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
+#define API_KEY @"your key"
+#define BASE_URL @"https://maps.googleapis.com/maps/api/place/search/json"
+
 @interface PlacesAPI : NSObject
+{
+    NSString *apiKey;
+    BOOL sensor;
+}
+
+@property (copy) NSString *apiKey;
+@property BOOL sensor;
+
+- (id)initWithApiKey:(NSString*)apiKey sensor:(BOOL) sensor;
+- (NSArray*)placeWithName:(NSString*)name AtLocation:(NSString*)location InRadius:(int)radius;
 
 @end
--- GoogleMapDemo/PlacesAPI.m
+++ GoogleMapDemo/PlacesAPI.m
@@ -7,7 +7,65 @@
 //
 
 #import "PlacesAPI.h"
+#import "Place.h"
+#import "CJSONDeserializer.h"
+#import "NSDictionary_JSONExtensions.h"
 
 @implementation PlacesAPI
 
+@synthesize apiKey;
+@synthesize sensor;
+
+- (id)initWithApiKey:(NSString*)anApiKey sensor:(BOOL)aSensor
+{
+    if (self = [super init]) {
+        PlacesAPI *api = (PlacesAPI*) self;
+        api.apiKey = anApiKey;
+        api.sensor = aSensor;
+    }
+    return self;
+}
+
+- (NSArray*)placeWithName:(NSString*)name AtLocation:(NSString*)location InRadius:(int)radius
+{
+    NSMutableArray *params = [[NSMutableArray alloc] initWithObjects:@"language=zh-TW", nil];
+    [params addObject:[NSString stringWithFormat:@"key=%@", self.apiKey]];
+    [params addObject:[NSString stringWithFormat:@"sensor=%@", self.sensor ? @"true": @"false"]];
+    [params addObject:[NSString stringWithFormat:@"location=%@", location]];
+    [params addObject:[NSString stringWithFormat:@"radius=%d", radius]];
+    if (name != nil) {
+        [params addObject:[NSString stringWithFormat:@"name=%@", [name stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]]];
+    }
+    
+    NSString *queryString = [params componentsJoinedByString:@"&"];
+    [params release];
+    
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@?%@", BASE_URL, queryString]];
+    NSLog(@"url: %@", url);
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    [request release];
+    
+    NSError *error = [[NSError alloc] init];
+    NSDictionary *json = [NSDictionary dictionaryWithJSONData: data error:&error];
+    [error release];
+    
+    NSString *status = [json objectForKey:@"status"];
+    NSMutableArray *retArray = [[[NSMutableArray alloc] init] autorelease];
+    if ([status isEqualToString:@"OK"]) {
+        NSArray *results = [json objectForKey: @"results"];
+        for (NSDictionary *item in results) {
+            Place *place = [[[Place alloc] initWithNSDictionary:item] autorelease];          
+            [retArray addObject:place];
+        }
+        
+        return [NSArray arrayWithArray:retArray]; 
+    }
+    
+    return nil;
+}
+
+- (void)dealloc {
+    [super dealloc];
+}
 @end
