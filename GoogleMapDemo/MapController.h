6df62d03036258086982e0961abb2a3491f1b8b6
Gasol Wu <gasol.wu@gmail.com>
2011-10-28 23:01:48 +0800

    new MapController

---
+++ GoogleMapDemo/MapController.h
@@ -0,0 +1,13 @@
+//
+//  MapController.h
+//  GoogleMapDemo
+//
+//  Created by Gasol Wu on 10/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapController : UIViewController
+
+@end
